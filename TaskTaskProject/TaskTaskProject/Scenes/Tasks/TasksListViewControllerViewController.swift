a6cb8ff97574389f7129da5f2157ff5d3bedc0f6
Eris Ramirez <kowayasky@gmail.com>
2022-08-21 23:14:16 -0500

    setupSearchTextField Constraints

--- TaskTaskProject/TaskTaskProject/Scenes/Tasks/TasksListViewControllerViewController.swift
+++ TaskTaskProject/TaskTaskProject/Scenes/Tasks/TasksListViewControllerViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 protocol TTPTasksListView: TTPView {}
 
@@ -14,14 +15,31 @@ final class TTPTasksListViewController: UIViewController {
     
     var presenter: TTPPresenter!
     
+    private lazy var searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Search"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         guard let presenter = presenter as? TTPTasksListPresenter else { return }
         presenter.viewDidLoad()
+        setupSearchTextField()
         // Do any additional setup after loading the view.
     }
     
+    private func setupSearchTextField() {
+        view.addSubview(searchTextField)
+        searchTextField.snp.makeConstraints { make in
+            make.topMargin.equalTo(10)
+            make.leading.equalTo(28)
+            make.trailing.equalTo(-28)
+            make.height.equalTo(50)
+        }
+    }
 }
 
 extension TTPTasksListViewController: TTPTasksListView {
